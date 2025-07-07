c25dcf65d06d262b995caaa7e060d2dffac05cf6
ciyek <aripurnomo606@gmail.com>
2025-07-07 22:36:32 +0700

    feat: implement AboutSection with content and styling

--- src/components/section/HomeSection/AboutSection.tsx
+++ src/components/section/HomeSection/AboutSection.tsx
@@ -1,7 +1,25 @@
+import Button from "@/components/ui/Button";
 import React from "react";
 
 const AboutSection = () => {
-  return <section className="h-screen w-screen">AboutSection</section>;
+  return (
+    <section className="relative h-screen w-screen">
+      <div className="absolute flex flex-col w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 className="text-[120px] text-[#322018] font-light text-center carattere leading-24">
+          Hard to Find,
+          <br /> hard to leave
+        </h1>
+        <p className="text-xl text-[#322018]/70 font-light text-center mt-4">
+          Just a few hours from the airport, on a green hill overlooking the
+          sea. A<br /> small resort with a big view and a great restaurant. Here
+          you find the true Bali.
+        </p>
+        <Button className="self-center mt-4 bg-[#903C02] hover:bg-[#b04f0f]">
+          Explore our village
+        </Button>
+      </div>
+    </section>
+  );
 };
 
 export default AboutSection;
