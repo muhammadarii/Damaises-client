127f2a782ea4cfa09e844a16b96754ed8da50a6a
ciyek <aripurnomo606@gmail.com>
2025-08-04 21:37:17 +0700

    feat: add Our Spa section with content and styling

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,6 +1,7 @@
 import AboutSection from "@/components/section/HomeSection/AboutSection";
 import HeroSection from "@/components/section/HomeSection/HeroSection";
 import OurRestaurantSection from "@/components/section/HomeSection/OurRestaurantSection";
+import OurSpaSection from "@/components/section/HomeSection/OurSpaSection";
 
 const HomePage = () => {
   return (
@@ -8,6 +9,7 @@ const HomePage = () => {
       <HeroSection />
       <AboutSection />
       <OurRestaurantSection />
+      <OurSpaSection />
     </>
   );
 };
