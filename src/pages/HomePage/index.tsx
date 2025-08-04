65f184645a9d7cc80c083de8e715a7e681c853fc
ciyek <aripurnomo606@gmail.com>
2025-08-04 21:16:27 +0700

    feat: add OurRestaurantSection component with layout and content

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,11 +1,13 @@
 import AboutSection from "@/components/section/HomeSection/AboutSection";
 import HeroSection from "@/components/section/HomeSection/HeroSection";
+import OurRestaurantSection from "@/components/section/HomeSection/OurRestaurantSection";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
       <AboutSection />
+      <OurRestaurantSection />
     </>
   );
 };
